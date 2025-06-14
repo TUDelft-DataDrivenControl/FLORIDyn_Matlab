d4f4d9c604a85cdc7694c474eea081148f46b1d4
Uwe Fechner <fechner@aenarete.eu>
2025-06-14 14:23:16 +0200

    Add test

--- test/test_windfield.m
+++ test/test_windfield.m
@@ -6,6 +6,7 @@
 %         @test ph ≈ 270.0
 %     end
 
+clear all
 addpath('./WindField/Direction_Constant');
 WindDir = 270;
 iT = [1, 2, 3];
@@ -18,6 +19,42 @@ for i = 1:length(phi)
 end
 rmpath('./WindField/Direction_Constant')
 
+%     dir_mode = Direction_Constant_wErrCov()
+%     struct WindDirType
+%         Data::Float64
+%         CholSig::Matrix{Float64}
+%     end
+%     WindDir = WindDirType(270.0, cholesky(Matrix{Float64}(I, 3, 3)).L)
+%     iT = [1, 2, 3]
+%     phi = getWindDirT(dir_mode, WindDir, iT)
+%     result = [269.6402710931765, 271.0872084924286, 269.5804103830612]
+%     for (i, ph) in pairs(phi)
+%         @test ph ≈ result[i]
+%     end
+
+clear all
+addpath('./WindField/Direction_Constant_wErrorCov');
+rng(1234);
+
+% Cholesky factor of 3x3 identity
+L = chol(eye(3), 'lower');
+
+% WindDir structure
+WindDir.Data = 270.0;
+WindDir.CholSig = L;
+
+iT = [1, 2, 3];
+
+% Expected result
+result = [269.6402710931765, 271.0872084924286, 269.5804103830612];
+
+phi = getWindDirT(WindDir, iT);
+for i = 1:length(phi)
+    assert(abs(phi(i) - result(i)) < 1e-8, 'Test failed at index %d', i);
+end
+disp('All tests passed.');
+
+rmpath('./WindField/Direction_Constant_wErrorCov');
 
 
 
